874f9364320d4a50e3c85e96b2130e5b8f5846ba
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-08 08:12:37 -0400

    add variable

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "app-name" {
+  default = "my-app"
+}
+
+variable "rg-location" {
+  default = "West Europe"
+}
+
+variable "web-github-url" {
+  default = ""
+}
+
+variable "web_github_pat" {
+  default = ""
+}
