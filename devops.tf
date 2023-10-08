0ea097d10dd9321309f473024de62dffbc5f9de2
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-08 08:13:32 -0400

    add azure devops, add project, service endpoint, and build definition

---
+++ devops.tf
@@ -0,0 +1,38 @@
+resource "azuredevops_project" "web" {
+  name               = "${var.app-name}-project"
+  visibility         = "private"
+  version_control    = "Git"
+  work_item_template = "Agile"
+  description        = "Project for ${var.app-name}"
+  features = {
+    "testplans" = "disabled"
+    "artifacts" = "disabled"
+  }
+}
+
+resource "azuredevops_serviceendpoint_github" "web-repo" {
+  project_id            = azuredevops_project.web.id
+  service_endpoint_name = "Example GitHub Personal Access Token"
+
+  auth_personal {
+    personal_access_token = var.web_github_pat
+  }
+}
+
+resource "azuredevops_build_definition" "web-build" {
+  project_id = azuredevops_project.web.id
+  name       = "Web Build Definition"
+  path       = "\\"
+
+  ci_trigger {
+    use_yaml = false
+  }
+
+  repository {
+    repo_type   = "GitHub"
+    repo_id     = var.web-github-url
+    yml_path    = "azure-pipelines.yml"
+    branch_name = "prod"
+    service_connection_id = azuredevops_serviceendpoint_github.web-repo.id
+  }
+}
