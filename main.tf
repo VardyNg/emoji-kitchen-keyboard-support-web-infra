95ec4e9edefd1b2d61153bb0d11fe52b3e7679e3
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-08 08:12:51 -0400

    add azuredevops provider

--- main.tf
+++ main.tf
@@ -4,10 +4,17 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.0.0"
     }
+    azuredevops = {
+      source = "microsoft/azuredevops"
+      version = "0.9.1"
+    }
   }
+}
 
 provider "azurerm" {
    features {}
 }
 
+provider "azuredevops" {
+  # Configuration options
 }
