9fc232023c3ea14a2558c480719378e8f8312fe0
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-08 08:12:14 -0400

    add blob storage resource, add storage account, storage container, and blob

---
+++ blob-storage.tf
@@ -0,0 +1,20 @@
+resource "azurerm_storage_account" "web-storage-account" {
+  name                     = "${var.app-name}sa"
+  resource_group_name      = azurerm_resource_group.default.name
+  location                 = azurerm_resource_group.default.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "web-storage-container" {
+  name                  = "${var.app-name}sc"
+  storage_account_name  = azurerm_storage_account.web-storage-account.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "web-storage-blob" {
+  name                   = "${var.app-name}sb"
+  storage_account_name   = azurerm_storage_account.web-storage-account.name
+  storage_container_name = azurerm_storage_container.web-storage-container.name
+  type                   = "Block"
+}
