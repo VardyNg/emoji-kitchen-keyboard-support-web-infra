1e6ead94dfd0d13c37511aa46f6a902fdbe0019b
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-09 12:40:00 -0400

    Revert "disable azurerm_cdn_endpoint"
    This reverts commit 28691499139c01120aa44260c61f898615f979ef.

--- cdn.tf
+++ cdn.tf
@@ -5,18 +5,17 @@ resource "azurerm_cdn_profile" "default" {
   sku                 = "Standard_Microsoft"
 }
 
-# resource "azurerm_cdn_endpoint" "default" {
-#   name                = "${var.app_name}-endpoint"
-#   profile_name        = azurerm_cdn_profile.default.name
-#   location            = azurerm_resource_group.default.location
-#   resource_group_name = azurerm_resource_group.default.name
+resource "azurerm_cdn_endpoint" "default" {
+  name                = "${var.app_name}-endpoint"
+  profile_name        = azurerm_cdn_profile.default.name
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
 
-#   origin {
-#     name      = var.app_name
-#     host_name = azurerm_storage_account.web-storage-account.primary_web_host
-#   }
-# }
-
+  origin {
+    name      = var.app_name
+    host_name = azurerm_storage_account.web-storage-account.primary_web_host
+  }
+}
 # resource "azurerm_cdn_endpoint_custom_domain" "default" {
 #   name            = "${var.app_name}-cdn-custom-domain"
 #   cdn_endpoint_id = azurerm_cdn_endpoint.default.id
