9c5a0a20b8eb158058ac6780874dbd09e0a6dd1e
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-09 12:34:00 -0400

    disable cloudflare_record

--- cloudflare.tf
+++ cloudflare.tf
@@ -1,7 +1,7 @@
-resource "cloudflare_record" "default" {
-  zone_id = var.cloudflare_zone_id
-  name    = local.hostname
-  value   = azurerm_cdn_endpoint.default.fqdn
-  type    = "CNAME"
-  ttl     = 3600
-}
+# resource "cloudflare_record" "default" {
+#   zone_id = var.cloudflare_zone_id
+#   name    = local.hostname
+#   value   = azurerm_cdn_endpoint.default.fqdn
+#   type    = "CNAME"
+#   ttl     = 3600
+# }
