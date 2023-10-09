1e284ff41641df7aae549b2d7f29760b4ba5b5ab
vardy.ng <wadee@wadeenghoiwa.com>
2023-10-08 23:14:48 -0400

    remove data.terraform_remote_state.state

--- main.tf
+++ main.tf
@@ -16,18 +16,6 @@ terraform {
 
   backend "azurerm" {}
 }
-
-data "terraform_remote_state" "state" {
-  backend = "azurerm"
-  config {
-    resource_group_name  = "emojikitchenkeyboard"
-    storage_account_name = "emojikitchenkeyboardsa"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    subscription_id      = var.subscription_id
-  }
-}
-
 provider "azurerm" {
    features {}
 }
